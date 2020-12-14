05ff4b7bd2e47ea04198661dd89e08a1712b7fe1
pedro <pfreitas@id.uff.br>
2020-12-14 17:16:28 -0300

    Initial Commit

---
+++ Dices-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dices-SwiftUI
+//
+//  Created by Pedro Freitas | Zoop on 14/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dices-SwiftUI/Dices_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dices_SwiftUIApp.swift
+//  Dices-SwiftUI
+//
+//  Created by Pedro Freitas | Zoop on 14/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Dices_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
